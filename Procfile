13fd87adc7468372c55da740f6bfe86f88e571f3
Nickolay.Varvonets <nickolay.varvonets@gmail.com>
2020-11-24 14:52:10 +0200

    Diploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Web_app.wsgi --log-file -
--- Web_app/settings.py
+++ Web_app/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,7 +28,7 @@ SECRET_KEY = 'y4xo!pflybnq-&z77+^_772p6z4#zk__h*pmxq!^al%r2%q@du'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,7 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
+
 STATIC_URL = '/static/'
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR , 'static')

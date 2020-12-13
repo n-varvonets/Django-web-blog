4d5572b851b036ad579837614a2bd63d128e2b6e
Nickolay.Varvonets <nickolay.varvonets@gmail.com>
2020-12-14 01:39:47 +0200

    refresh old links

--- Web_app/settings.py
+++ Web_app/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
+import whitenoise
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -118,8 +118,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
@@ -135,7 +133,4 @@ import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
-import django_heroku
-django_heroku.settings(locals())
 
